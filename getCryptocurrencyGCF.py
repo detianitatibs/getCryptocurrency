f94fda6bb7c0809f3da41cb29225f0d7245b5cbd
KeisukeTokuda <itati60031@gmail.com>
2021-01-16 01:59:47 +0900

    ソースコード追加。ISSUE有り。

---
+++ getCryptocurrencyGCF.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+"""
+GMOコインのAPIを利用して仮想通貨の値を取得して、
+Google Storageにtsvファイルとして保存する(GCF版)
+"""
+
+__author__ = "@detian_itatbs"
+__status__ = "development"
+__version__ = "0.0.1"
+__date__    = "16 January 2021"
+
+# ISSURE:functions-frameworkで別ファイルをimportする手段が分からない
+import getCryptocurrency
+
+def main(event, context): # for background functions
+    endpoint = 'https://api.coin.z.com/public'
+    path = '/v1/ticker'
+    dir_path = os.getcwd()
+    
+    crypto_dic = getCrypto(endpoint + path)
+    filepath = getFilepath(dir_path)
+
+    # ステータスが0(正常)かを確認する
+    if crypto_dic['status'] == 0:
+        createTsv(crypto_dic)
+    else:
+        # ログなどにエラーを出す際はこちらに書く
+        print("status error.")
+
+    print("終了時刻:"+getNowDtStr())
+    
